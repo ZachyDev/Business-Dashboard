3f66191c9577e03680d0765e8a592fbe60c5b13f
= <=>
2022-09-03 00:06:39 +0300

    added GlobalStyles

--- bizdash/src/Dashboard/Dashboard.js
+++ bizdash/src/Dashboard/Dashboard.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Brand from './Brand'
 import Container from './Container'
+import GlobalStyle from './GlobalStyle'
 function Dashboard() {
   return (
+    <>
+    <GlobalStyle />
     <Container>
         <Brand />
         <p>Normal text</p>
     </Container>
+    </>
   )
 }
 
